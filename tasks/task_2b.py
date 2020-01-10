7f9999a0909e8feb001ed49c3cbd826d7a857a6a
Jonas Scholz <jonas.scholz123@gmail.com>
2020-01-10 18:44:30 +0000

    included theoretical order of magnitude calculation

--- tasks/task_2b.py
+++ tasks/task_2b.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from model import OsloModel
 
 tc_values = {}
-nr_repetitions = 1
+nr_repetitions = 3
 system_sizes = [4, 8, 16, 32]
 total_iterations = 3000
 
@@ -22,11 +22,24 @@ for L in system_sizes:
 avg_tc_values = {k: np.average(v) for k,v in tc_values.items()}
 
 system_sizes = []
-avg_tcs      = []
+avg_tc_list  = []
 
 for L, avg_tc in avg_tc_values.items():
     system_sizes.append(L)
-    avg_tcs.append(avg_tc)
+    avg_tc_list.append(avg_tc)
 
-plt.loglog(system_sizes, avg_tcs, "x")
+plt.loglog(system_sizes, avg_tc_list, "x")
 plt.show()
+
+
+print(avg_tc_values)
+
+
+def theoretical_tc(L):
+    return 3/4 *L*(L+1)
+
+theoretical_values = {L : theoretical_tc(L) for L in system_sizes}
+print(theoretical_values)
+
+for L in system_sizes:
+    print(theoretical_values[L]/avg_tc_values[L])
