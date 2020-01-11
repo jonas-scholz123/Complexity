80873e2d380c5c02d6602e464355fdeb331d7f3a
Jonas Scholz <jonas.scholz123@gmail.com>
2020-01-11 13:55:46 +0000

    wrap script into functions

--- tasks/task_2b.py
+++ tasks/task_2b.py
@@ -5,41 +5,34 @@ sys.path.append("..") #allows import from parent level
 import matplotlib.pyplot as plt
 from model import OsloModel
 
-tc_values = {}
 nr_repetitions = 3
 system_sizes = [4, 8, 16, 32]
 total_iterations = 3000
 
-for L in system_sizes:
-    tc_values[L] = []
+def gather_data(system_sizes, total_iterations, nr_repetitions):
+    avg_tc_values = {}
+
+    for L in system_sizes:
+        avg_tc_values[L] = get_avg_tc(L, nr_repetitions)
+    return avg_tc_values
+
+def get_avg_tc(L, nr_repetitions):
+    tc_list = []
 
     for i in range(nr_repetitions):
 
         OM = OsloModel(L)
         OM.run(total_iterations)
-        tc_values[L].append(OM.first_drop_time)
+        tc_list.append(OM.first_drop_time)
 
-avg_tc_values = {k: np.average(v) for k,v in tc_values.items()}
+    return np.average(tc_list)
 
-system_sizes = []
-avg_tc_list  = []
+def display_data(datapoints):
+    x = list(datapoints.keys())
+    y = list(datapoints.values())
 
-for L, avg_tc in avg_tc_values.items():
-    system_sizes.append(L)
-    avg_tc_list.append(avg_tc)
+    plt.loglog(x, y, "x")
+    plt.show()
 
-plt.loglog(system_sizes, avg_tc_list, "x")
-plt.show()
-
-
-print(avg_tc_values)
-
-
-def theoretical_tc(L):
-    return 3/4 *L*(L+1)
-
-theoretical_values = {L : theoretical_tc(L) for L in system_sizes}
-print(theoretical_values)
-
-for L in system_sizes:
-    print(theoretical_values[L]/avg_tc_values[L])
+avg_tc_values = gather_data(system_sizes, total_iterations, nr_repetitions)
+display_data(avg_tc_values)
