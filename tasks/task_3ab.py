cea6ac22acebb2a06355f0517209207c890301a1
Jonas Scholz <jonas.scholz123@gmail.com>
2020-01-24 11:26:47 +0000

    Reduce number of variables to 1 through critical exponent relation

--- tasks/task_3ab.py
+++ tasks/task_3ab.py
@@ -28,7 +28,7 @@ def display_data(data, scale = 1):
     return
 
 
-system_sizes = [4, 8, 16, 32, 64, 128]
+system_sizes = [4, 8, 16, 32, 64]
 total_iterations = 30000
 
 #avalanche_sizes_dict = gather_data(system_sizes, total_iterations)
@@ -41,43 +41,51 @@ display_data(avalanche_sizes_dict, scale = 1.2)
 def power_law(s, A, tau):
     return A * 1/(s ** tau)
 
-def modify_data(x, y, tau, D, L):
+def log10_power_law(s, A, tau):
+    return np.log10(A) -tau*np.log10(s)
+
+def modify_data(x, y, tau, L):
+    D = 1/(2 - tau) # see paper for derivation
     collapsed_y = y * x**(tau)
     collapsed_x = x/(L**D)
     return collapsed_x, collapsed_y
 
-#avalanche_sizes = gather_data([L], 100000)
+def estimate_tau(avalanche_sizes, show_plot = False):
+    x, y = logbin(avalanche_sizes, 1.2)
 
-#fit, cov = curve_fit(power_law, x, y, p0 = [1.7, 1])
-#tau = fit[1]
-#A = fit[0]
-#tau = 1.427 # my value for tau
-tau = 1.5556 # accepted value for tau
-#fit_x = np.logspace(0, np.log10(max(x)), 50)
+    log_x = np.log10(x)
+    log_y = np.log10(y)
 
-#plt.loglog(x, y, "x")
-#plt.loglog(fit_x, power_law(fit_x, A, tau))
-#plt.show()
+    fit, cov = curve_fit(log10_power_law, log_x, log_y, p0 = [0.4, 1.55])
+    tau = fit[1]
+    print("initial estimated value of tau = ", tau)
+    if show_plot:
+        A = fit[0]
+        fit_x = np.logspace(0, np.log10(max(x)), 50)
 
-system_sizes = [4, 8, 16, 32, 64, 128]
-#avalanche_sizes = gather_data(system_sizes, 100000)
-
-#for D in [1.8, 1.9, 2, 2.1, 2.2]:
-D = 9/4 #accepted value for D
-for D in [2, 9/4]:
-    for tau in [1.427, 1.5556]:
-
-        for L in system_sizes:
-
-            x, y = logbin(avalanche_sizes[L], scale = 1.3, zeros = False)
-
-            #fit_x = np.logspace(0, np.log10(max(x)), 50)
-            #plt.loglog(x, y, "x")
-            #plt.loglog(fit_x, power_law(fit_x, A, tau))
-            #plt.show()
-
-            collapsed_x, collapsed_y = modify_data(x, y, tau=tau, D=D, L = L)
-            plt.loglog(collapsed_x, collapsed_y, "", label = "L = " + str(L))
-        plt.grid()
-        plt.legend()
+        plt.loglog(x, y, "x")
+        plt.loglog(fit_x, power_law(fit_x, A, tau))
         plt.show()
+    return tau
+
+system_sizes = [16, 32, 64, 128] #exclude small system sizes to uphold large L approximations made in derivations
+#avalanche_sizes_dict = gather_data(system_sizes, 100000)
+tau_0 = estimate_tau(avalanche_sizes_dict[max(system_sizes)])#estimate tau based on largest L
+
+#iterate over range of taus around tau_0 to find best collapse by inspection
+tau_range = np.linspace(0.95 * tau_0, 1.05 * tau_0, 20)
+for tau in tau_range:
+    for L in system_sizes:
+        x, y = logbin(avalanche_sizes_dict[L], scale = 1.3, zeros = False)
+
+        #fit_x = np.logspace(0, np.log10(max(x)), 50)
+        #plt.loglog(x, y, "x")
+        #plt.loglog(fit_x, power_law(fit_x, A, tau))
+        #plt.show()
+
+        collapsed_x, collapsed_y = modify_data(x, y, tau=tau, L = L)
+        plt.loglog(collapsed_x, collapsed_y, "x", label = "L = " + str(L))
+    plt.suptitle("tau = " + str(tau)[0:5])
+    plt.grid()
+    plt.legend()
+    plt.show()
